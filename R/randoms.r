c14e34d308de18e077165d50d87e47baf280c897
leondutoit <leon@comoyo.com>
2014-07-15 13:02:27 +0200

    add func for distinct fuzzy matching

--- R/randoms.r
+++ R/randoms.r
@@ -1,6 +1,16 @@
 
+#' @import stringr
+
 #' @export
 #' return a date from unix ts in milliseconds
 make_date <- function(x) {
   as.Date(as.POSIXct(x/1000, origin = "1970-01-01"))
+}
+
+#' @export
+#' fuzzy match on pattern and return distinct matches
+#' operates on a dataframe column
+distinct_fuzzy_match <- function(data, column_name, pattern) {
+  unique_entries <- unique(data[[column_name]])
+  unique_entries[str_detect(unique_entries, pattern)]
 }
