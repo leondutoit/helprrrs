577193ccfd17ea7122e7b2ac8fa71a9fbfef5cfc
leondutoit <leon@comoyo.com>
2014-07-15 13:05:11 +0200

    Clearer signature and implementation
    becuzz...

--- R/randoms.r
+++ R/randoms.r
@@ -10,7 +10,8 @@ make_date <- function(x) {
 #' @export
 #' fuzzy match on pattern and return distinct matches
 #' operates on a dataframe column
-distinct_fuzzy_match <- function(data, column_name, pattern) {
-  unique_entries <- unique(data[[column_name]])
-  unique_entries[str_detect(unique_entries, pattern)]
+distinct_fuzzy_match <- function(df, column_name, pattern) {
+  unique_entries <- unique(df[[column_name]])
+  matches <- str_detect(unique_entries, pattern)
+  unique_entries[matches]
 }
